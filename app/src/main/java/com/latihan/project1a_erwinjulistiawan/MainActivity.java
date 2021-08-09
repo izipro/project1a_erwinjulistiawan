4365d92a4fc65cf03051e72a66cb8db76df2aaf1
DESKTOP-UA86TFV\user <julistiawan56@gmail.com>
2021-08-09 22:19:39 +0700

    Penambahan Fungsi agar dapat menampilkan nama yang diinput oleh user

--- app/src/main/java/com/latihan/project1a_erwinjulistiawan/MainActivity.java
+++ app/src/main/java/com/latihan/project1a_erwinjulistiawan/MainActivity.java
@@ -3,12 +3,24 @@ package com.latihan.project1a_erwinjulistiawan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+EditText txtinputnama;
+TextView txthasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtinputnama=findViewById(R.id.txtinputnama);
+        txthasil=findViewById(R.id.txthasil);
+
     }
+   public void btntampil(View view){
+       txthasil.setText("Nama anda Adalah : "+txtinputnama.getText());
+   }
 }
